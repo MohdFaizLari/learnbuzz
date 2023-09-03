17e77cbb74cfccf8096838cf92da195db4eb78f0
mohdfaiz <mohdfaizlari@gmail.com>
2023-09-03 20:31:36 +0530

    login & signup button changed

--- javaScript/signup.js
+++ javaScript/signup.js
@@ -1,3 +1,20 @@
+function test() {
+  let testName = document.getElementById("fullName");
+  console.log(testName);
+}
+// let userDataStore = () => {
+//   if (document.getElementById("fullName")) {
+//     let fullName = document.getElementById("fullName");
+
+//     let innerText = fullName.innerText;
+//     console.log(e);
+//   }
+// let userName = document.getElementById("username").innerText;
+// let userEmail = document.getElementById("email").innerText;
+// let userPassword = document.getElementById("pwd").innerText;
+// };
+// userDataStore();
+
 if (document.getElementById("signupForm")) {
   let signUp = document.getElementById("signupForm");
   // Attach a submit event listener to the login form
@@ -6,4 +23,3 @@ if (document.getElementById("signupForm")) {
     window.location = "./../html/homepage.html";
   });
 }
-
