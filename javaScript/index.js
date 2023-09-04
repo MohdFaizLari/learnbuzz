ce186f17f8f91f278fb5039196acb21d8968db11
mohdfaiz <mohdfaizlari@gmail.com>
2023-09-04 13:37:33 +0530

    landing page JS code fixed

--- javaScript/index.js
+++ javaScript/index.js
@@ -16,14 +16,17 @@ let homePageRedirect = (e) => {
 
 let userDataSet = [];
 let userData = null;
+// console.log(userDataSet);
 let pageLoadSetup = (() => {
   // function body
   userData = JSON.parse(localStorage.getItem("userData"));
   if (userData !== null) {
     userDataSet.push(userData);
-    console.log(userDataSet);
+    // console.log(userDataSet);
   }
-  if (userDataSet.length>0) {
+  // console.log(userDataSet);
+  if (userDataSet.length > 0) {
+    console.log(userDataSet);
     localStorage.setItem("userDataSet", JSON.stringify(userDataSet));
   }
 })();
