8edd62d17279bc7ff136cf0ae6a3aa8ccbca456a
mohdfaiz <mohdfaizlari@gmail.com>
2023-09-04 16:36:20 +0530

    Doing some changes in user profile page

--- javaScript/user-profile.js
+++ javaScript/user-profile.js
@@ -3,15 +3,26 @@ let profilePageLoadSetup = (() => {
   userData = JSON.parse(localStorage.getItem("userData"));
   if (userData !== null) {
     let fullName = document.getElementById("fullname");
-    let userName = document.getElementById("username");
     let fullNameDiv = document.createElement("div");
+
+    let userName = document.getElementById("username");
     let userNameDiv = document.createElement("div");
 
+    let emailAddress = document.getElementById("email");
+    let userEmailDiv = document.createElement("div");
+
+    userEmailDiv.innerText = userData.email;
+    userEmailDiv.style.fontSize = "large";
+
     fullNameDiv.innerText = userData.fullname;
+    fullNameDiv.style.fontSize = "large";
+
     userNameDiv.innerText = userData.username;
+    userNameDiv.style.fontSize = "large";
 
     fullName.appendChild(fullNameDiv);
     userName.appendChild(userNameDiv);
+    emailAddress.appendChild(userEmailDiv)
 
     // console.log(userFullName.innerText);
   }
