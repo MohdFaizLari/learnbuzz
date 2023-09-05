34eacec2d08f4e0a36a8361606d3bd3f9c57f850
bushra <bushrafatima0901@gmail.com>
2023-09-05 22:22:48 +0530

    Courses Block of Code

--- javaScript/signup.js
+++ javaScript/signup.js
@@ -7,3 +7,58 @@ if (document.getElementById("signupForm")) {
   });
 }
 
+[
+  {
+    "id": 1,
+    "courseCategory1" :{
+          "Web Development":{
+            "course1":"Introduction to HTML and CSS",
+            "course2": "JavaScript Fundamentals",
+            "course3":"Full-Stack Web Development",
+            "course4":"Responsive Web Design"
+          }
+    },
+    "price": 499
+  },
+
+
+  {
+    "id": 2,
+    "courseCategory2" :{
+          "Digital Marketing":{
+            "course1":"Social Media Marketing",
+            "course2": "Search Engine Optimization (SEO)",
+            "course3":"Content Marketing Strategy",
+            "course4":"Email Marketing Essentials"
+          }
+    },
+    "price": 999
+  },
+
+  {
+    "id": 3,
+    "courseCategory3" :{
+          "Data Science":{
+            "course1":"Data Analysis with Python",
+            "course2": "Machine Learning Basics",
+            "course3":"Data Visualization with Tableau",
+            "course4":"Introduction to Data Analytics"
+          }
+    },
+    "price": 1299
+  },
+
+  {
+    "id": 4,
+    "courseCategory4" :{
+          "Graphic Design":{
+            "course1":"Adobe Photoshop Mastery",
+            "course2": "Graphic Design Principles",
+            "course3":"Logo Design Fundamentals",
+            "course4":"Typography for Designers"
+          }
+    },
+    "price": 1499
+  }
+ 
+]
