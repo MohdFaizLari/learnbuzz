dd5a97be187e8ac8e88e11211d470d61996053bf
mohdfaiz <mohdfaizlari@gmail.com>
2023-09-05 15:16:51 +0530

    User greeting added in profile page

--- javaScript/user-profile.js
+++ javaScript/user-profile.js
@@ -1,36 +1,42 @@
 let profilePageLoadSetup = (() => {
   let userData = {};
   userData = JSON.parse(localStorage.getItem("userData"));
+
   if (userData !== null) {
     let fullName = document.getElementById("fullname");
-    let fullNameDiv = document.createElement("div");
+    let userFullNameDiv = document.createElement("div");
+    userFullNameDiv.innerText = userData.fullname;
+    userFullNameDiv.style.fontSize = "large";
 
     let userName = document.getElementById("username");
-    let userNameDiv = document.createElement("div");
+    let userGivenNameDiv = document.createElement("div");
+    userGivenNameDiv.innerText = userData.username;
+    userGivenNameDiv.style.fontSize = "large";
 
     let emailAddress = document.getElementById("email");
     let userEmailDiv = document.createElement("div");
-
     userEmailDiv.innerText = userData.email;
     userEmailDiv.style.fontSize = "large";
 
-    fullNameDiv.innerText = userData.fullname;
-    fullNameDiv.style.fontSize = "large";
-
-    userNameDiv.innerText = userData.username;
-    userNameDiv.style.fontSize = "large";
-
+    let date = new Date();
+    let hoursTime = date.getHours();
     let userGreeting = document.getElementById("userGreeting");
-    userGreeting.innerText =
-      "Hey " + fullNameDiv.innerText + "," + " Have a nice Day!";
+    if (hoursTime >= 5 && hoursTime < 11.59) {
+      userGreeting.innerText =
+        "Hey " + fullNameDiv.innerText + "," + " Good morning!";
+    } else if (hoursTime >= 12 && hoursTime < 16.59) {
+      userGreeting.innerText =
+        "Hey " + userFullNameDiv.innerText + "," + " Good afternoon!";
+    } else if (hoursTime >= 17 && hoursTime < 23.59) {
+      userGreeting.innerText =
+        "Hey " + userFullNameDiv.innerText + "," + " Good eveining!";
+    }
 
     let userDescription = document.getElementById("userDescription");
-    userDescription.innerText = fullNameDiv.innerText;
-    
-    fullName.appendChild(fullNameDiv);
-    userName.appendChild(userNameDiv);
-    emailAddress.appendChild(userEmailDiv);
+    userDescription.innerText = userFullNameDiv.innerText;
 
-    // console.log(userFullName.innerText);
+    fullName.appendChild(userFullNameDiv);
+    userName.appendChild(userGivenNameDiv);
+    emailAddress.appendChild(userEmailDiv);
   }
 })();
