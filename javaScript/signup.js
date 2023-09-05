536ba15df7e7e53a4b1b1fef72531d2d293bc631
bushra <bushrafatima0901@gmail.com>
2023-09-05 22:35:57 +0530

    Editing Js file

--- javaScript/signup.js
+++ javaScript/signup.js
@@ -3,6 +3,19 @@ if (document.getElementById("signupForm")) {
   // Attach a submit event listener to the login form
   signUp.addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting normally
+    let userData = {};
+
+    let signupForm = document.forms.signupForm;
+    let fullName = signupForm.fullname.value;
+    userData.fullname = fullName;
+    let userName = signupForm.username.value;
+    userData.username = userName;
+    let email = signupForm.email.value;
+    userData.email = email;
+    let password = signupForm.pwd.value;
+    userData.password = password;
+
+    localStorage.setItem("userData", JSON.stringify(userData));
     window.location = "./../html/homepage.html";
   });
 }
