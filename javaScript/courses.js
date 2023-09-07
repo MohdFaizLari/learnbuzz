2b314ec648b61975dc4d1785f013100438b49992
bushra <bushrafatima0901@gmail.com>
2023-09-07 12:05:09 +0530

    Json Editing

--- javaScript/courses.js
+++ javaScript/courses.js
@@ -1,47 +1,48 @@
 let inputData = [
   {
-    id: 1,
-    coursePrice: 499,
-    courseCategory: "Web Development",
-    courses: [
-      { course1: "Introduction to HTML and CSS" },
-      { course1Link: "./../video/./Css/Tutorial__Introduction.mp4" },
-      { course2: "JavaScript Fundamentals" },
-      { course2Link: "../Media/production_id_4498849 (2160p).mp4" },
-      { course3: "Full-Stack Web Development" },
-      { course4: "Responsive Web Design" },
-    ],
+    "id": 1,
+    "coursePrice": 499,
+    "courseCategory":
+    {
+        "Web-Development":
+        [
+          {"course1":"Introduction to HTML & CSS",  "courseVideo1":"./../video/./html/_Introduction_to_HTML_(Hindi)(480p).mp4"},
+          {"course2":"JavaScript Fundamentals",  "courseVideo2":"video\html\_Tags_and_Element_in_HTML_(Hindi)(480p).mp4"},
+          {"course3":"Full Stack Web Development",  "courseVideo3":"video\html\_HTML_Tag_in_HTML_(Hindi)(480p).mp4"},
+          {"course4":"Responsive We Design",  "courseVideo3":"video\html\_Head_Tag_in_HTML_(Hindi)(480p).mp4"}
+        ]    
+    },
   },
 
-  {
-    id: 2,
-    courseCategory: "Digital Marketing",
-    coursePrice: 999,
-    course1: "Social Media Marketing",
-    course2: "Search Engine Optimization (SEO)",
-    course3: "Content Marketing Strategy",
-    course4: "Email Marketing Essentials",
-  },
+  // {
+  //   id: 2,
+  //   courseCategory: "Digital Marketing",
+  //   coursePrice: 999,
+  //   course1: "Social Media Marketing",
+  //   course2: "Search Engine Optimization (SEO)",
+  //   course3: "Content Marketing Strategy",
+  //   course4: "Email Marketing Essentials",
+  // },
 
-  {
-    id: 3,
-    courseCategory: "Data Science",
-    course1: "Data Analysis with Python",
-    course2: "Machine Learning Basics",
-    course3: "Data Visualization with Tableau",
-    course4: "Introduction to Data Analytics",
-    price: 1299,
-  },
+  // {
+  //   id: 3,
+  //   courseCategory: "Data Science",
+  //   course1: "Data Analysis with Python",
+  //   course2: "Machine Learning Basics",
+  //   course3: "Data Visualization with Tableau",
+  //   course4: "Introduction to Data Analytics",
+  //   price: 1299,
+  // },
 
-  {
-    id: 4,
-    courseCategory: "Graphic Design",
-    course1: "Adobe Photoshop Mastery",
-    course2: "Graphic Design Principles",
-    course3: "Logo Design Fundamentals",
-    course4: "Typography for Designers",
-    price: 1499,
-  },
+  // {
+  //   id: 4,
+  //   courseCategory: "Graphic Design",
+  //   course1: "Adobe Photoshop Mastery",
+  //   course2: "Graphic Design Principles",
+  //   course3: "Logo Design Fundamentals",
+  //   course4: "Typography for Designers",
+  //   price: 1499,
+  // },
 ];
 
 let coursesPageLoadSetup = (() => {
