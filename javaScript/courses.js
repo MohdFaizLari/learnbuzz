4f6d0f341d17277459f95a0fe881037feb6c0424
mohdfaiz <mohdfaizlari@gmail.com>
2023-09-09 00:12:47 +0530

    On click funvction added

--- javaScript/courses.js
+++ javaScript/courses.js
@@ -17,44 +17,43 @@ let inputData = [
         },
         {
           courseDescription: "Head Tag in HTML",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
-       
       ],
       css: [
         {
           courseDescription: "CSS Syntax",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./css/CSS_Syntax_(Hindi)(360p).mp4",
         },
         {
           courseDescription: "Element Selector",
-          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink:
+            "./../video/./css/Element_Selector_in_CSS_(Hindi)(480p).mp4",
         },
         {
           courseDescription: "Way of Inserting CSS",
-          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./css/Way_of_Inserting_CSS_(Hindi)(480p).mp4",
         },
-       
       ],
       javascript: [
         {
           courseDescription: "Alert Function in JS",
-          videoLink:"./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink:
+            "./../video/./javascript/alert_Function_in_JavaScript_(Hindi)(360p).mp4",
         },
         {
           courseDescription: "Function",
-          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink:
+            "./../video/./javascript/write_Function_in_JavaScript_(Hindi)(360p).mp4",
         },
         {
           courseDescription: "DOM in JS",
-          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink:
+            "./../video/./javascript/What_is_Document_Object_Model_in_JavaScript_(Hindi)(360p).mp4",
         },
       ],
     },
   },
-
 
   {
     id: 2,
@@ -74,16 +73,13 @@ let inputData = [
         },
         {
           courseDescription: "Head Tag in HTML",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
-       
       ],
       searchEngineOptimisation: [
         {
           courseDescription: "CSS Syntax",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
         {
           courseDescription: "Element Selector",
@@ -93,12 +89,11 @@ let inputData = [
           courseDescription: "Way of Inserting CSS",
           videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
-       
       ],
       contentMarketingStrategy: [
         {
           courseDescription: "Alert Function in JS",
-          videoLink:"./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
         {
           courseDescription: "Function",
@@ -130,16 +125,13 @@ let inputData = [
         },
         {
           courseDescription: "Head Tag in HTML",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
-       
       ],
       tableau: [
         {
           courseDescription: "CSS Syntax",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
         {
           courseDescription: "Element Selector",
@@ -149,12 +141,11 @@ let inputData = [
           courseDescription: "Way of Inserting CSS",
           videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
-       
       ],
       matlab: [
         {
           courseDescription: "Alert Function in JS",
-          videoLink:"./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
         {
           courseDescription: "Function",
@@ -186,16 +177,13 @@ let inputData = [
         },
         {
           courseDescription: "Head Tag in HTML",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
-       
       ],
       css: [
         {
           courseDescription: "CSS Syntax",
-          videoLink:
-            "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
         {
           courseDescription: "Element Selector",
@@ -205,12 +193,11 @@ let inputData = [
           courseDescription: "Way of Inserting CSS",
           videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
-       
       ],
       javascript: [
         {
           courseDescription: "Alert Function in JS",
-          videoLink:"./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
+          videoLink: "./../video/./html/5._Head_Tag_in_HTML_(Hindi)(480p).mp4",
         },
         {
           courseDescription: "Function",
@@ -225,68 +212,115 @@ let inputData = [
   },
 ];
 
+let test550 = () => {
+  for (let i = 0; i < inputData.length; i++) {
+    let coursesData = inputData[i].courses;
+    let courseCat = inputData[i].courseCategory;
 
+    let htmlCourse = coursesData.html;
+    let cssCourse = coursesData.css;
+    let jsCourse = coursesData.javascript;
+    if (!document.getElementById("webDevContainer")) {
+      if (htmlCourse !== null || "undefined") {
+        test551(coursesData, htmlCourse, courseCat);
+      }
+      if (cssCourse !== null || "undefined") {
+        test551(coursesData, cssCourse, courseCat);
+      }
+      if (jsCourse !== null || "undefined") {
+        test551(coursesData, jsCourse, courseCat);
+      }
+    }
+
+    // let test1 = inputData[i].courses.css;
+    // // console.log(test1);
+    // for (let j = 0; j < test1.length; j++) {
+    //   let videoscontainerDiv = document.createElement("div");
+    //   videoscontainerDiv.classList.add("videoscontainerDiv");
+    //   let videosContainer = document.createElement("video");
+    //   videosContainer.style.width = "100%";
+    //   videosContainer.style.height = "100%";
+    //   // videosContainer.autoplay = "disabled";
+    //   videosContainer.controls = "true";
+    //   videosContainer.muted = "true";
+    //   let cssCourseLink = test1[j].videoLink;
+    //   videosContainer.setAttribute("src", cssCourseLink);
+    //   videosContainer.setAttribute("type", "video/mp4");
+    //   videoscontainerDiv.appendChild(videosContainer);
+    //   mainContentContainer.appendChild(videoscontainerDiv);
+    // }
+
+    // let test2 = inputData[i].courses.javascript;
+    // // console.log(test2);
+    // for (let j = 0; j < test2.length; j++) {
+    //   let videoscontainerDiv = document.createElement("div");
+    //   videoscontainerDiv.classList.add("videoscontainerDiv");
+    //   let videosContainer = document.createElement("video");
+    //   videosContainer.style.width = "100%";
+    //   videosContainer.style.height = "100%";
+    //   // videosContainer.autoplay = "disabled";
+    //   videosContainer.controls = "true";
+    //   videosContainer.muted = "true";
+    //   let jsCourseLink = test2[j].videoLink;
+    //   videosContainer.setAttribute("src", jsCourseLink);
+    //   videosContainer.setAttribute("type", "video/mp4");
+    //   videoscontainerDiv.appendChild(videosContainer);
+    //   mainContentContainer.appendChild(videoscontainerDiv);
+    // }
+  }
+};
+
+let test551 = (coursesData, htmlCourse, courseCat) => {
+  let mainContentContainer = document.getElementById("mainContentContainer");
+  let courseVideosContainer = document.createElement("div");
+  courseVideosContainer.classList.add("coursesContainerStyling");
+
+  if ((courseCat = "Web-Developement")) {
+    courseVideosContainer.setAttribute("id", "webDevContainer");
+    if (!document.getElementById("Web-Developement")) {
+      mainContentContainer.appendChild(courseVideosContainer);
+    }
+  }
+  for (let j = 0; j < htmlCourse.length; j++) {
+    let videoscontainerDiv = document.createElement("div");
+    let courseVideos = htmlCourse[j].videoLink;
+    videoscontainerDiv.classList.add("videoscontainerDiv");
+    let videosContainer = document.createElement("video");
+    videosContainer.style.width = "100%";
+    videosContainer.style.height = "100%";
+    // videosContainer.autoplay = "disabled";
+    videosContainer.controls = "true";
+    videosContainer.muted = "true";
+    videosContainer.setAttribute("src", courseVideos);
+    videosContainer.setAttribute("type", "video/mp4");
+    videoscontainerDiv.appendChild(videosContainer);
+
+    courseVideosContainer.appendChild(videoscontainerDiv);
+  }
+};
 
 let coursesPageLoadSetup = (() => {
   let sideBarInnerContent = document.getElementById("sideBarContent");
   for (let i = 0; i < inputData.length; i++) {
     let inputDataObject = inputData[i];
     let sideBarCategories = document.createElement("div");
+    sideBarCategories.setAttribute("id", "courseCategories");
     sideBarCategories.innerText = inputDataObject.courseCategory;
     sideBarInnerContent.appendChild(sideBarCategories);
   }
 
-  let mainContentContainer = document.getElementById("mainContentContainer");
-  for (let i = 0; i < inputData.length; i++) {
-    let test = inputData[i].courses.html;
-    for (let j = 0; j < test.length; j++) {
-      let videoscontainerDiv = document.createElement("div");
-      videoscontainerDiv.classList.add("videoscontainerDiv");
-      let videosContainer = document.createElement("video");
-      videosContainer.style.width = "100%";
-      videosContainer.style.height = "100%";
-      videosContainer.autoplay = "disabled";
-      videosContainer.controls = "true";
-      let test1 = test[j].videoLink;
-      videosContainer.setAttribute("src", test1);
-      videosContainer.setAttribute("type", "video/mp4");
-      videoscontainerDiv.appendChild(videosContainer);
-      mainContentContainer.appendChild(videoscontainerDiv);
-    }
-  
-  
-    let test1 = inputData[i].courses.css;
-    console.log(test1);
-    for (let j = 0; j < test1.length; j++) {
-      let videoscontainerDiv = document.createElement("div");
-      videoscontainerDiv.classList.add("videoscontainerDiv");
-      let videosContainer = document.createElement("video");
-      videosContainer.style.width = "100%";
-      videosContainer.style.height = "100%";
-      videosContainer.autoplay = "disabled";
-      videosContainer.controls = "true";
-      let cssCourseLink = test1[j].videoLink;
-      videosContainer.setAttribute("src", cssCourseLink);
-      videosContainer.setAttribute("type", "video/mp4");
-      videoscontainerDiv.appendChild(videosContainer);
-      mainContentContainer.appendChild(videoscontainerDiv);
-    }
+  let courseCategories = document.getElementById("courseCategories");
 
-    let test2 = inputData[i].courses.javascript;
-    console.log(test2);
-    for (let j = 0; j < test2.length; j++) {
-      let videoscontainerDiv = document.createElement("div");
-      videoscontainerDiv.classList.add("videoscontainerDiv");
-      let videosContainer = document.createElement("video");
-      videosContainer.style.width = "100%";
-      videosContainer.style.height = "100%";
-      videosContainer.autoplay = "disabled";
-      videosContainer.controls = "true";
-      let jsCourseLink = test2[j].videoLink;
-      videosContainer.setAttribute("src", jsCourseLink);
-      videosContainer.setAttribute("type", "video/mp4");
-      videoscontainerDiv.appendChild(videosContainer);
-      mainContentContainer.appendChild(videoscontainerDiv);
-    }
+  if (courseCategories.innerText === "Web-Development") {
+    let webDevContainer = document.createElement("div");
+    courseCategories.addEventListener("click", test550);
   }
+
+  // if (courseCategories.innerText === "Digital Marketing") {
+  //   let digitalMarkContainer = document.createElement("div");
+  // } else if (courseCategories.innerText === "Data Science") {
+  //   let dataScContainer = document.createElement("div");
+  // } else if (courseCategories.innerText === "Graphic Design") {
+  //   let graphicDesignContainer = document.createElement("div");
+  // }
 })();
