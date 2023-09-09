ad585fcc905779c8cd0f394d485826e09377daea
mohdfaiz <mohdfaizlari@gmail.com>
2023-09-10 00:31:27 +0530

    User forget password done

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,45 +1,3 @@
-// let inputData = [
-//   {
-//     id: 1,
-//     courseCategory: "Web Development",
-//     course1: "Introduction to HTML and CSS",
-//     course2: "JavaScript Fundamentals",
-//     course3: "Full-Stack Web Development",
-//     course4: "Responsive Web Design",
-//     price: 499,
-//   },
-
-//   {
-//     id: 2,
-//     courseCategory: "Digital Marketing",
-//     course1: "Social Media Marketing",
-//     course2: "Search Engine Optimization (SEO)",
-//     course3: "Content Marketing Strategy",
-//     course4: "Email Marketing Essentials",
-//     price: 999,
-//   },
-
-//   {
-//     id: 3,
-//     courseCategory: "Data Science",
-//     course1: "Data Analysis with Python",
-//     course2: "Machine Learning Basics",
-//     course3: "Data Visualization with Tableau",
-//     course4: "Introduction to Data Analytics",
-//     price: 1299,
-//   },
-
-//   {
-//     id: 4,
-//     courseCategory: "Graphic Design",
-//     course1: "Adobe Photoshop Mastery",
-//     course2: "Graphic Design Principles",
-//     course3: "Logo Design Fundamentals",
-//     course4: "Typography for Designers",
-//     price: 1499,
-//   },
-// ];
-
 let loginButtonFunction = (e) => {
   window.location = "./../html/login.html";
 };
@@ -56,19 +14,19 @@ let homePageRedirect = (e) => {
   window.location = "./../html/homepage.html";
 };
 
-let userDataSet = [];
+// let userDataSet = [];
 let userData = null;
 // console.log(userDataSet);
 let pageLoadSetup = (() => {
-  // function body
-  userData = JSON.parse(localStorage.getItem("userData"));
+  userData = JSON.parse(localStorage.getItem("userDataSignUp"));
   if (userData !== null) {
-    userDataSet.push(userData);
-    // console.log(userDataSet);
+    localStorage.setItem("userModifiedData", JSON.stringify(userData));
   }
   // console.log(userDataSet);
-  if (userDataSet.length > 0) {
-    console.log(userDataSet);
-    localStorage.setItem("userDataSet", JSON.stringify(userDataSet));
-  }
 })();
+
+function passwordUpdate() {
+  usersUpdatedPassword = JSON.parse(localStorage.getItem("userUpdatedPassword"));
+  userData.password = usersUpdatedPassword;
+}
+
